bb156ec2bb241a3e0969b275c0816cfb8ae7310e
kurtsergey <kurtsergey@gmail.com>
2017-06-12 14:41:52 +0300

    + initial files

---
+++ index.js
@@ -0,0 +1,44 @@
+import jsrsasign from 'jsrsasign';
+import base64 from 'base-64';
+
+
+export default function verify(transactionDetails, publicKeyStr) {
+    var sig = new jsrsasign.KJUR.crypto.Signature({ alg: 'SHA1withRSA' }),
+        key = jsrsasign.KEYUTIL.getKey(formatPublicKey(publicKeyStr));
+
+    sig.init(key);
+    sig.updateString(transactionDetails.receiptData);
+
+    return sig.verify(base64toHEX(transactionDetails.receiptSignature));
+}
+
+
+function formatPublicKey(publicKeyStr) {
+    var chunkSize = 64,
+        chunks = [],
+        s = publicKeyStr;
+
+    while (s) {
+        chunks.push(s.substr(0, chunkSize));
+        s = s.substr(chunkSize);
+    }
+
+    s = chunks.join('\n');
+    s = '-----BEGIN PUBLIC KEY-----\n' + s + '\n-----END PUBLIC KEY-----';
+
+    return s;
+};
+
+
+function base64toHEX(s) {
+    var raw = base64.decode(s),
+        h,
+        res = '';
+
+    for (i = 0; i < raw.length; i++) {
+        h = raw.charCodeAt(i).toString(16);
+        res += h.length == 2 ? h : '0' + h;
+    }
+
+    return res.toUpperCase();
+}
